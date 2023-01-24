2cf3e19108a8d6b4aaabeb41e980d9570ff6ad49
eyahya-khan <eyahyakhan@gmail.com>
2023-01-24 11:36:11 +0100

    Fetch data

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
+import { useEffect,useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [data, setData] = useState([])
+  const [loading, setLoadoing] = useState(true)
+  const url = "http://cat-store-api.frostdigital.se/api";
+  useEffect(() =>{
+    fetch(url)
+    .then((res) => res.json())
+    .then((json) => setData(json))
+    .catch((error) => console.error(error))
+    .finally(() => setLoadoing(false))
+  },[])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {
+        loading ? (<Text>loading....</Text>) : (
+          data.products.map((data)=>(
+            <View>
+              <Text style={{fontSize:16}}>Title:{data.name}</Text>
+            </View>
+          ))
+        )
+      }
     </View>
   );
 }
